7e214137914b5b5060302edd7c88c96b5d57d426
CISC Student <cisc@ac.sdmesa.net>
2017-05-04 12:48:43 -0700

    Initial commit

---
+++ app/src/main/java/com/deitel/doodlz/MainActivity.java
@@ -0,0 +1,31 @@
+// MainActivity.java
+// Sets MainActivity's layout
+package com.deitel.doodlz;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      
+
+      int screenSize = 
+         getResources().getConfiguration().screenLayout & 
+         Configuration.SCREENLAYOUT_SIZE_MASK;
+
+      if (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+         setRequestedOrientation(
+            ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+      else
+         setRequestedOrientation(
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+   }
+}
+
